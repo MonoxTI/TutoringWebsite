ef1309c8740faa0fecd9f46c6bb9cd0d00e4c68d
MonoxTI <itumonokoane84@gmail.com>
2026-02-04 21:16:24 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),          // 👈 Essential for React + JSX
-    tailwindcss(),
-  ],
+  plugins: [react()],
 });
